560f0a95deecee8bfe408563740257d2f75e4722
evanbones <74276616+evanbones@users.noreply.github.com>
2026-05-16 16:07:04 -0700

    Remove map tweaks

--- mod/src/main/java/com/evandev/spicedcider/SpicedCider.java
+++ mod/src/main/java/com/evandev/spicedcider/SpicedCider.java
@@ -6,18 +6,15 @@ import com.evandev.spicedcider.config.LoggerNamePatternSelector;
 import com.evandev.spicedcider.config.Reconfigurator;
 import com.evandev.spicedcider.config.SpicedCiderConfig;
 import com.evandev.spicedcider.integration.cloth_config.SpicedCiderConfigScreen;
-import com.evandev.spicedcider.mixin.minecraft.accessor.MapColorAccessor;
 import com.evandev.spicedcider.namingunconvention.RandomNameGenerator;
 import com.evandev.spicedcider.registry.*;
 import com.evandev.spicedcider.resource.ResourceBaker;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.level.material.MapColor;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.fml.loading.FMLPaths;
 import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
@@ -56,7 +53,6 @@ public class SpicedCider {
 
         ResourceBaker.bakeFromManifest(cacheDir, manifestPath, resourcePacksDir);
 
-        modEventBus.addListener(this::setup);
         modEventBus.addListener(this::buildContents);
         modContainer.registerConfig(ModConfig.Type.COMMON, SpicedCiderConfig.COMMON_SPEC);
         modContainer.registerExtensionPoint(IConfigScreenFactory.class, (mc, screen) -> SpicedCiderConfigScreen.create(screen));
@@ -95,74 +91,6 @@ public class SpicedCider {
                 ModBlockEntities.WORKSTONE.get(),
                 (be, context) -> be.getInventory()
         );
-    }
-
-    private void setup(final FMLCommonSetupEvent event) {
-        event.enqueueWork(() -> {
-            ((MapColorAccessor) MapColor.GRASS).spicedcider$setCol(0x8EB971);
-            ((MapColorAccessor) MapColor.SAND).spicedcider$setCol(0xF7E9E3);
-            ((MapColorAccessor) MapColor.WOOL).spicedcider$setCol(0xFFFFFF);
-            ((MapColorAccessor) MapColor.FIRE).spicedcider$setCol(0xFF0000);
-            ((MapColorAccessor) MapColor.ICE).spicedcider$setCol(0xA0A0FF);
-            ((MapColorAccessor) MapColor.METAL).spicedcider$setCol(0xA7A7D7);
-            ((MapColorAccessor) MapColor.PLANT).spicedcider$setCol(0x507736);
-            ((MapColorAccessor) MapColor.SNOW).spicedcider$setCol(0xFFFFFF);
-            ((MapColorAccessor) MapColor.CLAY).spicedcider$setCol(0xA4A8B8);
-            ((MapColorAccessor) MapColor.DIRT).spicedcider$setCol(0xB6855B);
-            ((MapColorAccessor) MapColor.STONE).spicedcider$setCol(0x707070);
-            ((MapColorAccessor) MapColor.WATER).spicedcider$setCol(0x3F76E4);
-            ((MapColorAccessor) MapColor.WOOD).spicedcider$setCol(0xC0A361);
-            ((MapColorAccessor) MapColor.QUARTZ).spicedcider$setCol(0xFFFDF5);
-            ((MapColorAccessor) MapColor.COLOR_ORANGE).spicedcider$setCol(0xD87F33);
-            ((MapColorAccessor) MapColor.COLOR_MAGENTA).spicedcider$setCol(0xB24CD8);
-            ((MapColorAccessor) MapColor.COLOR_LIGHT_BLUE).spicedcider$setCol(0x6699D8);
-            ((MapColorAccessor) MapColor.COLOR_YELLOW).spicedcider$setCol(0xE5E533);
-            ((MapColorAccessor) MapColor.COLOR_LIGHT_GREEN).spicedcider$setCol(0x7FCC19);
-            ((MapColorAccessor) MapColor.COLOR_PINK).spicedcider$setCol(0xF27FA5);
-            ((MapColorAccessor) MapColor.COLOR_GRAY).spicedcider$setCol(0x4C4C4C);
-            ((MapColorAccessor) MapColor.COLOR_LIGHT_GRAY).spicedcider$setCol(0x999999);
-            ((MapColorAccessor) MapColor.COLOR_CYAN).spicedcider$setCol(0x4C7F99);
-            ((MapColorAccessor) MapColor.COLOR_PURPLE).spicedcider$setCol(0x7F3FB2);
-            ((MapColorAccessor) MapColor.COLOR_BLUE).spicedcider$setCol(0x334CB2);
-            ((MapColorAccessor) MapColor.COLOR_BROWN).spicedcider$setCol(0x664C33);
-            ((MapColorAccessor) MapColor.COLOR_GREEN).spicedcider$setCol(0x667F33);
-            ((MapColorAccessor) MapColor.COLOR_RED).spicedcider$setCol(0x993333);
-            ((MapColorAccessor) MapColor.COLOR_BLACK).spicedcider$setCol(0x191919);
-            ((MapColorAccessor) MapColor.GOLD).spicedcider$setCol(0xFAEE4D);
-            ((MapColorAccessor) MapColor.DIAMOND).spicedcider$setCol(0x5CD8D5);
-            ((MapColorAccessor) MapColor.LAPIS).spicedcider$setCol(0x4A80FF);
-            ((MapColorAccessor) MapColor.EMERALD).spicedcider$setCol(0x00D93A);
-            ((MapColorAccessor) MapColor.PODZOL).spicedcider$setCol(0x815631);
-            ((MapColorAccessor) MapColor.NETHER).spicedcider$setCol(0x700200);
-            ((MapColorAccessor) MapColor.TERRACOTTA_WHITE).spicedcider$setCol(0xD1B1A1);
-            ((MapColorAccessor) MapColor.TERRACOTTA_ORANGE).spicedcider$setCol(0x9F5224);
-            ((MapColorAccessor) MapColor.TERRACOTTA_MAGENTA).spicedcider$setCol(0x95576C);
-            ((MapColorAccessor) MapColor.TERRACOTTA_LIGHT_BLUE).spicedcider$setCol(0x706C8A);
-            ((MapColorAccessor) MapColor.TERRACOTTA_YELLOW).spicedcider$setCol(0xBA8524);
-            ((MapColorAccessor) MapColor.TERRACOTTA_LIGHT_GREEN).spicedcider$setCol(0x677535);
-            ((MapColorAccessor) MapColor.TERRACOTTA_PINK).spicedcider$setCol(0xA04D4E);
-            ((MapColorAccessor) MapColor.TERRACOTTA_GRAY).spicedcider$setCol(0x392923);
-            ((MapColorAccessor) MapColor.TERRACOTTA_LIGHT_GRAY).spicedcider$setCol(0x876A62);
-            ((MapColorAccessor) MapColor.TERRACOTTA_CYAN).spicedcider$setCol(0x575C5C);
-            ((MapColorAccessor) MapColor.TERRACOTTA_PURPLE).spicedcider$setCol(0x7A4958);
-            ((MapColorAccessor) MapColor.TERRACOTTA_BLUE).spicedcider$setCol(0x4C3E7C);
-            ((MapColorAccessor) MapColor.TERRACOTTA_BROWN).spicedcider$setCol(0x4C3223);
-            ((MapColorAccessor) MapColor.TERRACOTTA_GREEN).spicedcider$setCol(0x4C522A);
-            ((MapColorAccessor) MapColor.TERRACOTTA_RED).spicedcider$setCol(0x8E3C2E);
-            ((MapColorAccessor) MapColor.TERRACOTTA_BLACK).spicedcider$setCol(0x251610);
-            ((MapColorAccessor) MapColor.CRIMSON_NYLIUM).spicedcider$setCol(0xBD3031);
-            ((MapColorAccessor) MapColor.CRIMSON_STEM).spicedcider$setCol(0x5C191D);
-            ((MapColorAccessor) MapColor.CRIMSON_HYPHAE).spicedcider$setCol(0x5C191D);
-            ((MapColorAccessor) MapColor.WARPED_NYLIUM).spicedcider$setCol(0x167E86);
-            ((MapColorAccessor) MapColor.WARPED_STEM).spicedcider$setCol(0x3A8E8C);
-            ((MapColorAccessor) MapColor.WARPED_HYPHAE).spicedcider$setCol(0x14B485);
-            ((MapColorAccessor) MapColor.WARPED_WART_BLOCK).spicedcider$setCol(0x167E86);
-            ((MapColorAccessor) MapColor.DEEPSLATE).spicedcider$setCol(0x646464);
-            ((MapColorAccessor) MapColor.RAW_IRON).spicedcider$setCol(0xD8AF93);
-            ((MapColorAccessor) MapColor.GLOW_LICHEN).spicedcider$setCol(0x7FA796);
-
-            LOGGER.info("Successfully injected nicer map colors!");
-        });
     }
 
     @SubscribeEvent
