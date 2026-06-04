eefddcfc5fead031fcd199b1d12fc5a846942369
evanbones <74276616+evanbones@users.noreply.github.com>
2026-06-03 20:48:02 -0700

    thank you cassian, my goal

--- mod/src/main/java/com/evandev/spicedcider/client/events/ClientGameEvents.java
+++ mod/src/main/java/com/evandev/spicedcider/client/events/ClientGameEvents.java
@@ -3,11 +3,20 @@ package com.evandev.spicedcider.client.events;
 import com.evandev.spicedcider.SpicedCider;
 import com.evandev.spicedcider.client.music.DeathSoundInstance;
 import com.evandev.spicedcider.registry.ModSounds;
+import dev.emi.emi.api.EmiApi;
+import dev.emi.emi.api.stack.EmiIngredient;
+import dev.emi.emi.api.stack.EmiStack;
+import dev.emi.emi.api.stack.EmiStackInteraction;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.client.event.ScreenEvent;
+import org.lwjgl.glfw.GLFW;
 
 @EventBusSubscriber(modid = SpicedCider.MOD_ID, bus = EventBusSubscriber.Bus.GAME, value = Dist.CLIENT)
 public class ClientGameEvents {
@@ -27,6 +36,31 @@ public class ClientGameEvents {
             wasDead = isDead;
         } else {
             wasDead = false;
+        }
+    }
+
+    @SubscribeEvent
+    public static void onKeyPressed(ScreenEvent.KeyPressed.Pre event) {
+        if (FMLLoader.isProduction() || event.getKeyCode() != GLFW.GLFW_KEY_C) {
+            return;
+        }
+
+        EmiStackInteraction interaction = EmiApi.getHoveredStack(true);
+
+        if (interaction != null && !interaction.isEmpty()) {
+            EmiIngredient ingredient = interaction.getStack();
+
+            if (!ingredient.isEmpty() && !ingredient.getEmiStacks().isEmpty()) {
+                EmiStack emiStack = ingredient.getEmiStacks().getFirst();
+                ItemStack stack = emiStack.getItemStack();
+
+                if (stack != null && !stack.isEmpty()) {
+                    String descriptionId = stack.getItem().getDescriptionId();
+                    String clipboardData = "\"%s\": \"%s\"".formatted(descriptionId, I18n.get(descriptionId));
+
+                    Minecraft.getInstance().keyboardHandler.setClipboard(clipboardData);
+                }
+            }
         }
     }
 }
