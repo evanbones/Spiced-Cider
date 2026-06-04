23bbc7e0f5081587bdb502b0e3e66906f6bb5c45
evanbones <74276616+evanbones@users.noreply.github.com>
2026-06-03 20:33:16 -0700

    Cleanup ranged attack goal

--- mod/src/main/java/com/evandev/spicedcider/goals/SimpleRangedAttackGoal.java
+++ mod/src/main/java/com/evandev/spicedcider/goals/SimpleRangedAttackGoal.java
@@ -11,6 +11,7 @@ import java.util.function.BiConsumer;
 import java.util.function.Predicate;
 
 public class SimpleRangedAttackGoal<T extends Mob> extends Goal {
+
     protected final T mob;
     protected final BiConsumer<T, LivingEntity> performRangedAttack;
     protected final Predicate<ItemStack> weaponPredicate;
@@ -19,16 +20,17 @@ public class SimpleRangedAttackGoal<T extends Mob> extends Goal {
     protected final int attackIntervalMax;
     protected final float attackRadius;
     protected final float attackRadiusSqr;
+
     protected LivingEntity target;
     protected int attackTime = -1;
     protected int seeTime;
 
-    public SimpleRangedAttackGoal(T p_i1649_1_, Predicate<ItemStack> weaponPredicate, BiConsumer<T, LivingEntity> performRangedAttack, double speedModifier, int attackInterval, float attackRadius) {
-        this(p_i1649_1_, weaponPredicate, performRangedAttack, speedModifier, attackInterval, attackInterval, attackRadius);
+    public SimpleRangedAttackGoal(T mob, Predicate<ItemStack> weaponPredicate, BiConsumer<T, LivingEntity> performRangedAttack, double speedModifier, int attackInterval, float attackRadius) {
+        this(mob, weaponPredicate, performRangedAttack, speedModifier, attackInterval, attackInterval, attackRadius);
     }
 
-    public SimpleRangedAttackGoal(T p_i1650_1_, Predicate<ItemStack> weaponPredicate, BiConsumer<T, LivingEntity> performRangedAttack, double speedModifier, int attackIntervalMin, int attackIntervalMax, float attackRadius) {
-        this.mob = p_i1650_1_;
+    public SimpleRangedAttackGoal(T mob, Predicate<ItemStack> weaponPredicate, BiConsumer<T, LivingEntity> performRangedAttack, double speedModifier, int attackIntervalMin, int attackIntervalMax, float attackRadius) {
+        this.mob = mob;
         this.weaponPredicate = weaponPredicate;
         this.performRangedAttack = performRangedAttack;
         this.speedModifier = speedModifier;
@@ -39,19 +41,22 @@ public class SimpleRangedAttackGoal<T extends Mob> extends Goal {
         this.setFlags(EnumSet.of(Flag.MOVE, Flag.LOOK));
     }
 
+    @Override
     public boolean canUse() {
         if (!this.mob.isHolding(this.weaponPredicate)) {
             return false;
         }
-        LivingEntity livingentity = this.mob.getTarget();
-        if (livingentity != null && livingentity.isAlive()) {
-            this.target = livingentity;
+
+        LivingEntity currentTarget = this.mob.getTarget();
+        if (currentTarget != null && currentTarget.isAlive()) {
+            this.target = currentTarget;
             return true;
         } else {
             return false;
         }
     }
 
+    @Override
     public boolean canContinueToUse() {
         if (!this.mob.isHolding(this.weaponPredicate)) {
             return false;
@@ -59,45 +64,47 @@ public class SimpleRangedAttackGoal<T extends Mob> extends Goal {
         return this.canUse() || !this.mob.getNavigation().isDone();
     }
 
+    @Override
     public void stop() {
         this.target = null;
         this.seeTime = 0;
         this.attackTime = -1;
     }
 
+    @Override
     public boolean requiresUpdateEveryTick() {
         return true;
     }
 
+    @Override
     public void tick() {
-        double d0 = this.mob.distanceToSqr(this.target.getX(), this.target.getY(), this.target.getZ());
-        boolean flag = this.mob.getSensing().hasLineOfSight(this.target);
-        if (flag) {
+        double distanceSqr = this.mob.distanceToSqr(this.target.getX(), this.target.getY(), this.target.getZ());
+        boolean hasLineOfSight = this.mob.getSensing().hasLineOfSight(this.target);
+
+        if (hasLineOfSight) {
             ++this.seeTime;
         } else {
             this.seeTime = 0;
         }
 
-        if (!(d0 > (double) this.attackRadiusSqr) && this.seeTime >= 5) {
+        if (distanceSqr <= (double) this.attackRadiusSqr && this.seeTime >= 5) {
             this.mob.getNavigation().stop();
         } else {
             this.mob.getNavigation().moveTo(this.target, this.speedModifier);
         }
 
         this.mob.getLookControl().setLookAt(this.target, 30.0F, 30.0F);
+
         if (--this.attackTime == 0) {
-            if (!flag) {
+            if (!hasLineOfSight) {
                 return;
             }
 
-            float f = Mth.sqrt((float) d0) / this.attackRadius;
-            float lvt_5_1_ = Mth.clamp(f, 0.1F, 1.0F);
+            float distanceRatio = Mth.sqrt((float) distanceSqr) / this.attackRadius;
             this.performRangedAttack.accept(this.mob, this.target);
-            this.attackTime = Mth.floor(f * (float) (this.attackIntervalMax - this.attackIntervalMin) + (float) this.attackIntervalMin);
+            this.attackTime = Mth.floor(distanceRatio * (float) (this.attackIntervalMax - this.attackIntervalMin) + (float) this.attackIntervalMin);
         } else if (this.attackTime < 0) {
-            float f2 = Mth.sqrt((float) d0) / this.attackRadius;
-            this.attackTime = Mth.floor(Mth.lerp(Math.sqrt(d0) / (double) this.attackRadius, (double) this.attackIntervalMin, (double) this.attackIntervalMax));
+            this.attackTime = Mth.floor(Mth.lerp(Math.sqrt(distanceSqr) / (double) this.attackRadius, this.attackIntervalMin, this.attackIntervalMax));
         }
-
     }
 }
